14cce6f4746d51681b19253e7e05a91e3130b1a7
Salah Merouani <salah.merouani@univ-constantine2.dz>
2023-08-24 15:46:17 +0100

    Update nomachinewindows10.sh

--- nomachinewindows10.sh
+++ nomachinewindows10.sh
@@ -18,22 +18,12 @@ function goto {
 
 : ngrok
 clear
-echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+
 CRP="$TOKEN"
 ./ngrok authtoken $CRP
 
 clear
-echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
-echo "======================="
-echo "choose ngrok region (for better connection)."
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
+
 CRP="$LANGUAGE"
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 
