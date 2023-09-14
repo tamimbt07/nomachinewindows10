a18bcd0f2402f00053e2e0812ce50852851391b9
Salah Merouani <sala7merouani@gmail.com>
2023-09-14 17:57:35 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -31,7 +31,8 @@ CRP="$REGION"  # Change to your desired region
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g i5ala7/darkubuntu:v3
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
