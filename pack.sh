68e5ea7f83889d9f5d88d2c2bf22ab0d37dafd3c
Salah Merouani <sala7merouani@gmail.com>
2023-09-15 18:05:48 +0100

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/user/ &&
 echo '123456' | sudo -S wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb &&
-mkdir .config &&
+
 cd .config && wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zsb9UHbguHlnXj53gXK1zbKymi013AS4" -O google-chrome-profile.tar.bz2 && rm -rf /tmp/cookies.txt && tar -xvf google-chrome-profile.tar.bz2 && exit
