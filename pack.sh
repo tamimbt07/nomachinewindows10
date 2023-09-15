45d139b18b36c05004266c68da3d442fca517811
Salah Merouani <sala7merouani@gmail.com>
2023-09-15 16:05:52 +0100

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-echo '123456' | sudo -S wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && cd .config && wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zsb9UHbguHlnXj53gXK1zbKymi013AS4" -O google-chrome-profile.tar.bz2 && rm -rf /tmp/cookies.txt && tar -xvf google-chrome-profile.tar.bz2  && xfconf-query -c keyboard-layout -p /Default/XkbLayout -s fr,ar --create -t string && xfce4-keyboard-settings && exit
+cd /home/user/ &&
+echo '123456' | sudo -S wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+mkdir .config &&
+cd .config && wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zsb9UHbguHlnXj53gXK1zbKymi013AS4" -O google-chrome-profile.tar.bz2 && rm -rf /tmp/cookies.txt && tar -xvf google-chrome-profile.tar.bz2 && exit
